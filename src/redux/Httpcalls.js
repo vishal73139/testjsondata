32100baeaaef1436351100b9f805f131c862da72
Rama Krishna Teja <rk.dchandrahas@gmail.com>
2021-02-22 22:54:46 +0530

    exception and adjustment changes

--- src/redux/Httpcalls.js
+++ src/redux/Httpcalls.js
@@ -5,3 +5,5 @@ const newAxiosInstance = axios.create();
 export const getRules = newAxiosInstance.get('https://datapurereposervicenew.azurewebsites.net/getRules');
 
 export const saveRules = (postdata) => newAxiosInstance.post('https://datapurereposervicenew.azurewebsites.net/saveRule',postdata,{"Access-Control-Allow-Origin":"*"});
+
+export const getExceptionType = (postData) => newAxiosInstance.post('https://datapurereposervicenew.azurewebsites.net/getExceptionType',postData,{"Access-Control-Allow-Origin":"*"});
