8a0fa4626b3d1c3f2e8c9072fe39ff5877b79027
vishal sharma <vishalrocks73139@gmail.com>
2021-02-22 22:58:22 +0530

    view rule screen changes

--- src/redux/Httpcalls.js
+++ src/redux/Httpcalls.js
@@ -5,3 +5,5 @@ const newAxiosInstance = axios.create();
 export const getRules = newAxiosInstance.get('https://datapurereposervicenew.azurewebsites.net/getRules');
 
 export const saveRules = (postdata) => newAxiosInstance.post('https://datapurereposervicenew.azurewebsites.net/saveRule',postdata,{"Access-Control-Allow-Origin":"*"});
+
+export const executeRules = (processDate,ruleId,versionId) => newAxiosInstance.get('https://datapurereposervicenew.azurewebsites.net/executeRule?injectionDate='+processDate+'&ruleId='+ruleId+'&versionId='+versionId);
