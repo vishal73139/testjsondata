fc1b7762998fba056ed786bda9f6b29ed3e36234
vishal sharma <vishalrocks73139@gmail.com>
2021-02-25 17:40:03 +0530

    fixed exception data

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4a0f57186fc48d63262e536cbfaf8b0f.js"
+  "./precache-manifest.1438cf924555b45fc8a44bc863db2456.js"
 );
 
 self.addEventListener('message', (event) => {
